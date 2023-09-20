ce1737b5c497b45b1c41b82bc047bd04d599a712
RendonnM <srendonm@correo.iue.edu.co>
2023-09-20 18:54:41 -0500

    Entrega 2 - V8- 19-9-2023
    Se agregan funcionalidades al bingo. HTML, CSS y JS

--- challenges/resources/bingo/bingo.js
+++ challenges/resources/bingo/bingo.js
@@ -1,48 +1,67 @@
-function getBingoCard() {
-    // Crear arreglo con subarreglo para cada columna necesaria
-    let arr = [
-        [], // b (1-15)
-        [], // i (16-30)
-        [], // n (31-45)
-        [], // g (46-60)
-        []  // o (51-75)
-    ];
-    // Llenar cada subarreglo
-    for (let i = 0; i < arr.length; i++) {
-        // Asignar máximo y mínimo de acuerdo a posición
-        let min = (i * 15) + 1;
-        let max = min + 15;
-        // Este ciclo termina cuando el subarreglo tenga 5 elementos
-        while (arr[i].length < 5) {
-            let num = Math.floor(Math.random() * (max - min)) + min;
-            // Evitar que se repitan números
-            if (!arr[i].includes(num)) {
-                arr[i].push(num);
-            }
-        }
-        // Ordenar
-        arr[i].sort((a, b) => a - b);
+const inputSubmit = document.getElementById('startgame');
+
+inputSubmit.addEventListener('click', function () {
+
+    const game = document.getElementById('game');
+    if (game) {
+        game.style.display = 'block';
     }
-    // Generalmente el número del centro es un comodín
-    arr[2][2] = 'X';
-    return arr;
-}
+    const numrivales = document.getElementById('numrivales').value;
+    const numtarjetas = document.getElementById('numtarjetas').value;
+    console.log('Empieza el juego')
 
-function cartones() {
-    return [
-        getBingoCard(),
-        getBingoCard(),
-        getBingoCard(),
-        getBingoCard(),
-        getBingoCard(),
-        getBingoCard()
-    ]
-}
+    function getBingoCard() {
 
-let cards = cartones();
-let html = '';
-cards.forEach(card => {
-    html += `
+        let arr = [
+            [], // b (1-15)
+            [], // i (16-30)
+            [], // n (31-45)
+            [], // g (46-60)
+            []  // o (51-75)
+        ];
+        for (let i = 0; i < arr.length; i++) {
+            let min = (i * 15) + 1;
+            let max = min + 15;
+            while (arr[i].length < 5) {
+                let num = Math.floor(Math.random() * (max - min)) + min;
+                if (!arr[i].includes(num)) {
+                    arr[i].push(num);
+                }
+            }
+            arr[i].sort((a, b) => a - b);
+        }
+        arr[2][2] = '';
+        return arr;
+    }
+
+    function cartones() {
+
+        return [
+            getBingoCard()
+        ]
+    }
+
+    function cartones2() {
+
+        return [
+            getBingoCard()
+        ]
+    }
+
+    let cards = cartones();
+    let cardsrivales = cartones2();
+
+    for (let i = 1; i < numtarjetas; i++) {
+        cards.push(getBingoCard());
+    }
+
+    for (let i = 1; i < numrivales; i++) {
+        cardsrivales.push(getBingoCard());
+    }
+
+    let html = '';
+    cards.forEach(card => {
+        html += `
       <table>
       <thead>
       <tr>
@@ -54,43 +73,176 @@ cards.forEach(card => {
       </tr>
       </thead>
       <tbody>`;
-    for (let i = 0; i < 5; i++) {
-        html += `
+        for (let i = 0; i < 5; i++) {
+            html += `
+          <tr>`;
+            for (let j = 0; j < 5; j++) {
+                const numero = card[j][i];
+                let claseNumero = `n${numero}`;
+
+                if (numero === bola) {
+                    claseNumero += ' tachado';
+                }
+
+                html += `
+            <td class="${claseNumero}">${numero}</td>`;
+            }
+            html += `
+          </tr>`;
+        }
+        html += '</tbody></table>';
+    });
+    document.querySelector('#bingo-cards').innerHTML = html;
+
+    let html2 = '';
+    cardsrivales.forEach(card => {
+        html2 += `
+      <table>
+      <thead>
       <tr>
-        <td>${card[0][i]}</td>
-        <td>${card[1][i]}</td>
-        <td>${card[2][i]}</td>
-        <td>${card[3][i]}</td>
-        <td>${card[4][i]}</td>
+        <th>B</th>
+        <th>I</th>
+        <th>N</th>
+        <th>G</th>
+        <th>O</th>
       </tr>
-        `;
+      </thead>
+      <tbody>`;
+        for (let i = 0; i < 5; i++) {
+            html2 += `
+          <tr>`;
+            for (let j = 0; j < 5; j++) {
+                const numero = card[j][i];
+                let claseNumero = `n${numero}`;
+
+                if (numero === bola) {
+                    claseNumero += ' tachado';
+                }
+
+                html2 += `
+            <td class="${claseNumero}">${numero}</td>`;
+            }
+            html2 += `
+          </tr>`;
+        }
+        html2 += '</tbody></table>';
+    });
+    document.querySelector('#bingo-cards2').innerHTML = html2;
+
+    let newCards = [];
+    for (let i = 0; i < cards.length; i++) {
+        let simple = [];
+        for (x = 0; x < 5; x++) {
+            simple.push(...cards[i][x]);
+        }
+        newCards.push(simple);
     }
-    html += '</tbody></table>';
+    console.log(newCards);
+
+    const setFontColor = (element) => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        element.style.backgroundColor = "#" + randomColor;
+    };
+
+    const tables = document.querySelectorAll('table');
+    tables.forEach((table) => {
+        setFontColor(table);
+    });
+
+
+    var rango = Array.from({ length: 75 }, (_, i) => i + 1);
+    for (let i = rango.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [rango[i], rango[j]] = [rango[j], rango[i]];
+    }
+
+    let balotasSalientes = [];
+    function sacarBalota() {
+        let bola = Math.floor(Math.random() * rango.length);
+        bola = rango[bola];
+
+        for (var i = rango.length - 1; i >= 0; i--) {
+            if (rango[i] === bola) {
+                rango.splice(i, 1);
+            }
+        }
+
+        console.log(rango);
+
+        let divbola = document.getElementById('bola');
+        divbola.textContent = bola;
+        balotasSalientes.push(bola);
+
+        let numerosConClase = document.querySelectorAll(`.n${bola}`);
+        for (var i = 0; i < numerosConClase.length; ++i) {
+            let numero = numerosConClase[i];
+            numero.classList.add('tachado');
+
+            console.log(numero)
+        }
+    }
+
+    function tacharNumerosEnCartones(cartones, numero) {
+        cartones.forEach(card => {
+            const celdas = card.querySelectorAll('td');
+            celdas.forEach(celda => {
+                const numeroCelda = parseInt(celda.textContent);
+                if (numeroCelda === numero) {
+                    celda.classList.add('tachado');
+                }
+            });
+        });
+    }
+
+    let ganadorEncontrado = false;
+    function verificarGanador(cartones) {
+        for (let i = 0; i < cartones.length; i++) {
+            const carton = cartones[i];
+            let cartonCompleto = true;
+
+            for (let columna = 0; columna < carton.length; columna++) {
+                for (let fila = 0; fila < carton[columna].length; fila++) {
+                    if (columna === 2 && fila === 2) {
+                        continue;
+                    }
+
+                    const numero = carton[columna][fila];
+                    if (!balotasSalientes.includes(numero)) {
+                        cartonCompleto = false;
+                        break;
+                    }
+                }
+                if (!cartonCompleto) {
+                    break;
+                }
+            }
+
+            if (cartonCompleto) {
+                ganadorEncontrado = true;
+
+                const mensajeganador = document.getElementById('ganador-mensaje');
+                mensajeganador.style.display = 'block';
+
+                console.log("¡Tenemos un ganador! El cartón está completo.");
+                break;
+            }
+        }
+
+        if (ganadorEncontrado) {
+            console.log("¡Tenemos un ganador! El cartón está completo.");
+        } else {
+            console.log("Ningún ganador todavía");
+        }
+    }
+
+    const intervalId = setInterval(function () {
+        sacarBalota();
+        verificarGanador(cards);
+        verificarGanador(cardsrivales);
+        if (ganadorEncontrado) {
+            clearInterval(intervalId);
+            console.log("¡Tenemos un ganador!");
+        }
+    }, 50);
+
 });
-document.querySelector('#bingo-cards').innerHTML = html;
-
-// Si quieres que se muestren como arreglo, sin subarreglos:
-let newCards = [];
-for (let i = 0; i < cards.length; i++) {
-    // Crear arreglo para tarjeta
-    let simple = [];
-    for (x = 0; x < 5; x++) {
-        simple.push(...cards[i][x]);
-    }
-    newCards.push(simple);
-}
-console.log(newCards);
-
-// Función para establecer un color de fuente aleatorio
-const setFontColor = (element) => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    element.style.backgroundColor = "#" + randomColor;
-  };
-  
-  // Selecciona todas las tablas de bingo por su etiqueta 'table'
-  const tables = document.querySelectorAll('table');
-  
-  // Aplica un color de fuente aleatorio a cada tabla de bingo
-  tables.forEach((table) => {
-    setFontColor(table);
-  });
